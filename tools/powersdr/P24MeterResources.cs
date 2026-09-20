2f50095d5ecb60407bf9441beb617ddc6b78a7a3
SQ4KOU <sq4kou@wp.pl>
2026-09-20 16:21:14 +0200

    P24 native port: return exact Thetis resources as Bitmap

--- tools/powersdr/P24MeterResources.cs
+++ tools/powersdr/P24MeterResources.cs
@@ -8,7 +8,7 @@ namespace PowerSDR
     {
         private static readonly string Root = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "P24ThetisMeterResources");
 
-        internal static Image Get(string name)
+        internal static Bitmap Get(string name)
         {
             try
             {
@@ -21,20 +21,20 @@ namespace PowerSDR
             catch { return null; }
         }
 
-        internal static Image gear { get { return Get("gear"); } }
-        internal static Image pin_not_on_top { get { return Get("pin_not_on_top"); } }
-        internal static Image pin_on_top { get { return Get("pin_on_top"); } }
-        internal static Image dot { get { return Get("dot"); } }
-        internal static Image dockIcon_dock { get { return Get("dockIcon_dock"); } }
-        internal static Image dockIcon_float { get { return Get("dockIcon_float"); } }
-        internal static Image resizegrab { get { return Get("resizegrab"); } }
-        internal static Image arrow_left { get { return Get("arrow_left"); } }
-        internal static Image arrow_topleft { get { return Get("arrow_topleft"); } }
-        internal static Image arrow_up { get { return Get("arrow_up"); } }
-        internal static Image arrow_topright { get { return Get("arrow_topright"); } }
-        internal static Image arrow_right { get { return Get("arrow_right"); } }
-        internal static Image arrow_bottomright { get { return Get("arrow_bottomright"); } }
-        internal static Image down { get { return Get("down"); } }
-        internal static Image arrow_bottomleft { get { return Get("arrow_bottomleft"); } }
+        internal static Bitmap gear { get { return Get("gear"); } }
+        internal static Bitmap pin_not_on_top { get { return Get("pin_not_on_top"); } }
+        internal static Bitmap pin_on_top { get { return Get("pin_on_top"); } }
+        internal static Bitmap dot { get { return Get("dot"); } }
+        internal static Bitmap dockIcon_dock { get { return Get("dockIcon_dock"); } }
+        internal static Bitmap dockIcon_float { get { return Get("dockIcon_float"); } }
+        internal static Bitmap resizegrab { get { return Get("resizegrab"); } }
+        internal static Bitmap arrow_left { get { return Get("arrow_left"); } }
+        internal static Bitmap arrow_topleft { get { return Get("arrow_topleft"); } }
+        internal static Bitmap arrow_up { get { return Get("arrow_up"); } }
+        internal static Bitmap arrow_topright { get { return Get("arrow_topright"); } }
+        internal static Bitmap arrow_right { get { return Get("arrow_right"); } }
+        internal static Bitmap arrow_bottomright { get { return Get("arrow_bottomright"); } }
+        internal static Bitmap down { get { return Get("down"); } }
+        internal static Bitmap arrow_bottomleft { get { return Get("arrow_bottomleft"); } }
     }
 }
