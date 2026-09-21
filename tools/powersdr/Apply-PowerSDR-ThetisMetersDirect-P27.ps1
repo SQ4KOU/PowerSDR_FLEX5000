deb91b937b76a62c0687853892994cbc42cddc02
SQ4KOU <sq4kou@wp.pl>
2026-09-21 23:46:43 +0200

    P27: package exact Thetis configuration icon resources

--- tools/powersdr/Apply-PowerSDR-ThetisMetersDirect-P27.ps1
+++ tools/powersdr/Apply-PowerSDR-ThetisMetersDirect-P27.ps1
@@ -20,6 +20,34 @@ if(!(Test-Path $srcConfig)){throw "P27 config source missing: $srcConfig"}
 Copy-Item $srcConfig $dstConfig -Force
 
 $bridge=[IO.File]::ReadAllText($bridgePath)
+# Exact Thetis configuration-button graphics.
+$resourcePropertyAnchor='        public static Image resizegrab { get { return Load("resizegrab"); } }'
+if(!$bridge.Contains($resourcePropertyAnchor)){throw 'P27 P25MeterResources anchor missing'}
+$resourceProperties=@'
+        public static Image arrow_left_black { get { return Load("arrow_left_black"); } }
+        public static Image arrow_right_black { get { return Load("arrow_right_black"); } }
+        public static Image arrow_up_black { get { return Load("arrow_up_black"); } }
+        public static Image down_black { get { return Load("down_black"); } }
+        public static Image pipette32 { get { return Load("pipette32"); } }
+        public static Image brush32 { get { return Load("brush32"); } }
+'@
+$bridge=$bridge.Replace($resourcePropertyAnchor,$resourcePropertyAnchor+$nl+$resourceProperties)
+
+$resDir=Join-Path $consoleDir 'Resources'
+New-Item -ItemType Directory -Force -Path $resDir | Out-Null
+$resourceUrls=@{
+    'arrow_left_black'='https://raw.githubusercontent.com/ramdor/Thetis/8220ec089451380054e9c31313d4ac2d4bf11776/Project%20Files/Source/Console/Resources/arrow_left_black.png'
+    'arrow_right_black'='https://raw.githubusercontent.com/ramdor/Thetis/8220ec089451380054e9c31313d4ac2d4bf11776/Project%20Files/Source/Console/Resources/arrow_right_black.png'
+    'arrow_up_black'='https://raw.githubusercontent.com/ramdor/Thetis/8220ec089451380054e9c31313d4ac2d4bf11776/Project%20Files/Source/Console/Resources/arrow_up_black.png'
+    'down_black'='https://raw.githubusercontent.com/ramdor/Thetis/8220ec089451380054e9c31313d4ac2d4bf11776/Project%20Files/Source/Console/Resources/down_black.png'
+    'pipette32'='https://raw.githubusercontent.com/ramdor/Thetis/49df874df78c472b9a3fda2a20f413768390d441/Project%20Files/Source/Console/Resources/pipette32.png'
+    'brush32'='https://raw.githubusercontent.com/ramdor/Thetis/49df874df78c472b9a3fda2a20f413768390d441/Project%20Files/Source/Console/Resources/brush32.png'
+}
+foreach($r in $resourceUrls.Keys)
+{
+    Invoke-WebRequest -UseBasicParsing -Uri $resourceUrls[$r] -OutFile (Join-Path $resDir ($r+'.png'))
+}
+
 
 $field='        private ToolStripMenuItem p25AddRx1Menu;'
 if(!$bridge.Contains($field)){throw 'P27 bridge menu field anchor missing'}
@@ -64,6 +92,22 @@ if(!$proj.Contains('<Compile Include="P27ThetisMetersConfigForm.cs" />'))
 {
     $proj=$proj.Replace($anchor,$anchor+$nl+'    <Compile Include="P27ThetisMetersConfigForm.cs" />')
 }
+
+$resourceNames=@('arrow_left_black','arrow_right_black','arrow_up_black','down_black','pipette32','brush32')
+foreach($r in $resourceNames)
+{
+    $rel='Resources\'+$r+'.png'
+    if($proj -notmatch ('Content Include="'+[regex]::Escape($rel)+'"'))
+    {
+        $item='    <Content Include="'+$rel+'">'+$nl+
+              '      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>'+$nl+
+              '    </Content>'+$nl
+        $igPos=$proj.IndexOf('</ItemGroup>')
+        if($igPos -lt 0){throw 'P27 csproj ItemGroup close missing'}
+        $proj=$proj.Insert($igPos,$item)
+    }
+}
+
 [IO.File]::WriteAllText($projPath,$proj,$utf8NoBom)
 
 $verifyBridge=[IO.File]::ReadAllText($bridgePath)
@@ -92,6 +136,12 @@ foreach($token in @(
     'CheckBoxTS chkMeterItemDarkMode',
     'btnMeterCopySettings',
     'btnMeterPasteSettings',
+    'arrow_left_black',
+    'arrow_right_black',
+    'arrow_up_black',
+    'down_black',
+    'pipette32',
+    'brush32',
     'GetSettingsForMeterGroup',
     'ApplySettingsForMeterGroup',
     'SetOrderForMeterType',
