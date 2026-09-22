c417e2699d87c1d7d83c80a7a8011a8104421694
SQ4KOU <sq4kou@wp.pl>
2026-09-22 23:02:49 +0200

    P38: preserve P27 menu anchor while applying visible top-menu color

--- tools/powersdr/P25ThetisMetersBridge.cs
+++ tools/powersdr/P25ThetisMetersBridge.cs
@@ -227,12 +227,12 @@ namespace PowerSDR
             MeterManager.FinishSetupAndDisplay();
 
             p25MetersMenu = new ToolStripMenuItem("Meters/Gadgets");
-            if (menuStrip1.Items.Count > 0)
-                p25MetersMenu.ForeColor = menuStrip1.Items[0].ForeColor;
             p25AddRx1Menu = new ToolStripMenuItem("Add RX1 Signal Meter");
             p25AddRx1Menu.Click += delegate { P25AddRx1SignalMeter(); };
             p25MetersMenu.DropDownItems.Add(p25AddRx1Menu);
             menuStrip1.Items.Add(p25MetersMenu);
+            if (menuStrip1.Items.Count > 1)
+                p25MetersMenu.ForeColor = menuStrip1.Items[0].ForeColor;
         }
 
         private string P25AddRx1SignalMeter()
